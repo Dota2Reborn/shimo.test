b60062632d2f6e602199968c7b9b58f3f36c3dec
Dota2Reborn <zhuiyaoyan@126.com>
2017-12-01 16:42:40 +0800

    update

--- src/test/testAddCollaborator.java
+++ src/test/testAddCollaborator.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -166,12 +167,16 @@ public class testAddCollaborator {
 		login("autoTest01@shimo.im", "123123");
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
 
 		Actions action = new Actions(driver);
 		action.contextClick(desktop1_1).perform();
 		desktop_setting_doc_5.click();
 
-		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator_close));
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_2_list));
 		Boolean exist = button_addCollaborator.isEnabled();
 
 		assertFalse(exist);
@@ -189,6 +194,10 @@ public class testAddCollaborator {
 		login("autoTest01@shimo.im", "123123");
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
 
 		Actions action = new Actions(driver);
 		action.contextClick(desktop1_1_folder).perform();
@@ -219,10 +228,317 @@ public class testAddCollaborator {
 	}
 
 	/**
+	 * 通过【最近】列表中的第一个添加为协作者
+	 * 
+	 * @author 刘晨
+	 * @Time 2017-11-29
+	 *
+	 */
+	@Test(enabled = true)
+	public void addCollaborator_3() throws InterruptedException {
+		login("autoTest01@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		Actions action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		String msg_a = addCollaborator_total.getText();
+		button_addCollaborator.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(addCollaborator_1_add));
+		addCollaborator_1_add.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_ok));
+		b_addCollaborator_ok.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		b_addCollaborator_2_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_4));
+		list_addCollaborator_4.click();
+
+		Thread.sleep(500);
+		String msg_b = addCollaborator_total.getText();
+
+		assertEquals(msg_a, msg_b);
+	}
+
+	/**
+	 * 通过【企业成员】列表中的第二个添加为协作者
+	 * 
+	 * @author 刘晨
+	 * @Time 2017-11-29
+	 *
+	 */
+	@Test(enabled = true)
+	public void addCollaborator_4() throws InterruptedException {
+		login("autoTest01@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		Actions action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		String msg_a = addCollaborator_total.getText();
+		button_addCollaborator.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(addCollaborator_company_list));
+		addCollaborator_company_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(addCollaborator_companyList_2_add));
+		addCollaborator_companyList_2_add.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_ok));
+		b_addCollaborator_ok.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		b_addCollaborator_2_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_4));
+		list_addCollaborator_4.click();
+
+		Thread.sleep(500);
+		String msg_b = addCollaborator_total.getText();
+
+		assertEquals(msg_a, msg_b);
+	}
+
+	/**
+	 * 点击协作者列表中添加协作者之后，不点击确定，点击返回，协作者不会被添加
+	 * 
+	 * @author 刘晨
+	 * @Time 2017-11-29
+	 *
+	 */
+	@Test(enabled = true)
+	public void addCollaborator_5() throws InterruptedException {
+		login("autoTest01@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		Actions action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		String msg_a = addCollaborator_total.getText();
+		button_addCollaborator.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(addCollaborator_1_add));
+		addCollaborator_1_add.click();
+
+		b_addCollaborator_back.click();
+		Thread.sleep(500);
+		String msg_b = addCollaborator_total.getText();
+
+		assertEquals(msg_a, msg_b);
+	}
+
+	/**
+	 * 协作者在协作者列表中移除自己
+	 * 
+	 * @author 刘晨
+	 * @Time 2017-12-01
+	 *
+	 */
+	@Test(enabled = true)
+	public void addCollaborator_6() throws InterruptedException {
+		login("autoTest01@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		Actions action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		button_addCollaborator.click();
+
+		input_addCollaborator.sendKeys("autoTest04@shimo.im");
+		Thread.sleep(500);
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_add));
+		b_addCollaborator_1_add.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_ok));
+		b_addCollaborator_ok.click();
+
+		logout();
+		login("autoTest04@shimo.im", "123123");
+
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg1 = desktop_show_type.getText();
+		if (msg1.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_2_list));
+		b_addCollaborator_2_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_4));
+		list_addCollaborator_4.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_confirm));
+		b_addCollaborator_confirm.click();
+
+		Thread.sleep(500);
+		String fileName = desktop1_1_folder.getText();
+		assertNotEquals(fileName, "添加协作者测试");
+	}
+
+	/**
+	 * 作为协作者添加协作者
+	 * 
+	 * @author 刘晨
+	 * @Time 2017-12-01
+	 *
+	 */
+	@Test(enabled = true)
+	public void addCollaborator_7() throws InterruptedException {
+		login("autoTest03@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		Actions action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(button_addCollaborator));
+		button_addCollaborator.click();
+
+		input_addCollaborator.sendKeys("autoTest04@shimo.im");
+		Thread.sleep(500);
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_add));
+		b_addCollaborator_1_add.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_ok));
+		b_addCollaborator_ok.click();
+
+		logout();
+		login("autoTest04@shimo.im", "123123");
+
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg1 = desktop_show_type.getText();
+		if (msg1.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_2_list));
+		b_addCollaborator_2_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_4));
+		list_addCollaborator_4.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_confirm));
+		b_addCollaborator_confirm.click();
+
+		Thread.sleep(500);
+		String fileName = desktop1_1_folder.getText();
+		assertNotEquals(fileName, "添加协作者测试");
+	}
+
+	/**
+	 * 企业成员之间转让所有权
+	 * 
+	 * @author 刘晨
+	 * @Time 2017-12-01
+	 *
+	 */
+	@Test(enabled = true)
+	public void addCollaborator_8() throws InterruptedException {
+		login("autoTest01@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg = desktop_show_type.getText();
+		if (msg.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		Actions action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_list));
+		b_addCollaborator_1_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_1));
+		list_addCollaborator_1.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_changeOwner_1));
+		list_addCollaborator_changeOwner_1.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_confirm));
+		b_addCollaborator_confirm.click();
+
+		logout();
+		login("autoTest03@shimo.im", "123123");
+		desktop.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+		String msg1 = desktop_show_type.getText();
+		if (msg1.equals("平铺")) {
+			desktop_show_type.click();
+		}
+
+		action = new Actions(driver);
+		action.contextClick(desktop1_1_folder).perform();
+		desktop_setting_doc_5.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_list));
+		b_addCollaborator_1_list.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_1));
+		list_addCollaborator_1.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(list_addCollaborator_changeOwner_1));
+		list_addCollaborator_changeOwner_1.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_confirm));
+		b_addCollaborator_confirm.click();
+		wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_list));
+
+		String email = addCollaborator_1_list_userEmail.getText();
+		assertEquals(email, "autoTest01");
+	}
+
+	/**
 	 * 桌面排序as更新时间
 	 * 
 	 * @author 刘晨
-	 * @Time 2017-11-21
+	 * @Time 2017-11-28
 	 *
 	 */
 	@Test(enabled = true)
@@ -231,7 +547,10 @@ public class testAddCollaborator {
 
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
-		desktop_show_type.click();
+		String msg = desktop_show_type.getText();
+		if (msg.equals("列表")) {
+			desktop_show_type.click();
+		}
 		desktop_order.click();
 
 		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByUpdate));
@@ -241,15 +560,16 @@ public class testAddCollaborator {
 		Boolean f2 = desktop_list_2_folder.getText().equals("最新创建");
 		Boolean f3 = desktop_list_3_folder.getText().equals("111");
 
-		assertEquals(true, f1 && f2 && f3);
+		desktop_show_type.click();
+		assertTrue(f1 && f2 && f3);
 
 	}
-	
+
 	/**
 	 * 桌面排序as创建时间
 	 * 
 	 * @author 刘晨
-	 * @Time 2017-11-21
+	 * @Time 2017-11-28
 	 *
 	 */
 	@Test(enabled = true)
@@ -258,7 +578,10 @@ public class testAddCollaborator {
 
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
-		desktop_show_type.click();
+		String msg = desktop_show_type.getText();
+		if (msg.equals("列表")) {
+			desktop_show_type.click();
+		}
 		desktop_order.click();
 
 		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByCreate));
@@ -268,15 +591,16 @@ public class testAddCollaborator {
 		Boolean f2 = desktop_list_2_folder.getText().equals("111");
 		Boolean f3 = desktop_list_3_folder.getText().equals("最近更新");
 
-		assertEquals(true, f1 && f2 && f3);
+		desktop_show_type.click();
+		assertTrue(f1 && f2 && f3);
 
 	}
-	
+
 	/**
 	 * 桌面排序as文件名
 	 * 
 	 * @author 刘晨
-	 * @Time 2017-11-21
+	 * @Time 2017-11-28
 	 *
 	 */
 	@Test(enabled = true)
@@ -285,7 +609,10 @@ public class testAddCollaborator {
 
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
-		desktop_show_type.click();
+		String msg = desktop_show_type.getText();
+		if (msg.equals("列表")) {
+			desktop_show_type.click();
+		}
 		desktop_order.click();
 
 		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByFile));
@@ -295,15 +622,16 @@ public class testAddCollaborator {
 		Boolean f2 = desktop_list_2_folder.getText().equals("所有者排序");
 		Boolean f3 = desktop_list_3_folder.getText().equals("文件夹排序");
 
-		assertEquals(true, f1 && f2 && f3);
+		desktop_show_type.click();
+		assertTrue(f1 && f2 && f3);
 
 	}
-	
+
 	/**
 	 * 桌面排序as所有者
 	 * 
 	 * @author 刘晨
-	 * @Time 2017-11-21
+	 * @Time 2017-11-28
 	 *
 	 */
 	@Test(enabled = true)
@@ -312,7 +640,10 @@ public class testAddCollaborator {
 
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
-		desktop_show_type.click();
+		String msg = desktop_show_type.getText();
+		if (msg.equals("列表")) {
+			desktop_show_type.click();
+		}
 		desktop_order.click();
 
 		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByOwner));
@@ -322,10 +653,11 @@ public class testAddCollaborator {
 		Boolean f2 = desktop_list_2_folder.getText().equals("最新创建");
 		Boolean f3 = desktop_list_3_folder.getText().equals("111");
 
-		assertEquals(true, f1 && f2 && f3);
+		desktop_show_type.click();
+		assertTrue(f1 && f2 && f3);
 
 	}
-	
+
 	/**
 	 * 桌面排序as文件夹置顶
 	 * 
@@ -339,17 +671,30 @@ public class testAddCollaborator {
 
 		desktop.click();
 		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
-		desktop_show_type.click();
+		String msg = desktop_show_type.getText();
+		if (msg.equals("列表")) {
+			desktop_show_type.click();
+		}
 		desktop_order.click();
 
 		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByFolderUP));
 		desktop_orderByFolderUP.click();
 
+		desktop_order.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByDefault));
+		desktop_orderByDefault.click();
+
 		Boolean f1 = desktop_list_1_folder.getText().equals("文件夹排序");
 		Boolean f2 = desktop_list_2_folder.getText().equals("最新创建");
 		Boolean f3 = desktop_list_3_folder.getText().equals("111");
 
-		assertEquals(true, f1 && f2 && f3);
+		desktop_order.click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(desktop_orderByFolderUP));
+		desktop_orderByFolderUP.click();
+
+		desktop_show_type.click();
+		assertTrue(f1 && f2 && f3);
 
 	}
 
@@ -391,8 +736,8 @@ public class testAddCollaborator {
 	public WebElement desktop_orderByOwner;
 	@SearchWith(pageName = "desktop", elementName = "desktop_orderByFolderUP")
 	public WebElement desktop_orderByFolderUP;
-	
-	
+	@SearchWith(pageName = "desktop", elementName = "desktop_orderByDefault")
+	public WebElement desktop_orderByDefault;
 
 	@SearchWith(pageName = "homePage", elementName = "userEmail")
 	public WebElement userEmail;
@@ -411,12 +756,33 @@ public class testAddCollaborator {
 	public WebElement input_addCollaborator;
 	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_1_add")
 	public WebElement b_addCollaborator_1_add;
+	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_1_list")
+	public WebElement b_addCollaborator_1_list;
 	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_2_list")
 	public WebElement b_addCollaborator_2_list;
+	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_3_list")
+	public WebElement b_addCollaborator_3_list;
+	@SearchWith(pageName = "addCollaborator", elementName = "list_addCollaborator_1")
+	public WebElement list_addCollaborator_1;
 	@SearchWith(pageName = "addCollaborator", elementName = "list_addCollaborator_4")
 	public WebElement list_addCollaborator_4;
 	@SearchWith(pageName = "addCollaborator", elementName = "addCollaborator_total")
 	public WebElement addCollaborator_total;
 	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_ok")
 	public WebElement b_addCollaborator_ok;
+	@SearchWith(pageName = "addCollaborator", elementName = "addCollaborator_1_add")
+	public WebElement addCollaborator_1_add;
+	@SearchWith(pageName = "addCollaborator", elementName = "addCollaborator_companyList_2_add")
+	public WebElement addCollaborator_companyList_2_add;
+	@SearchWith(pageName = "addCollaborator", elementName = "addCollaborator_company_list")
+	public WebElement addCollaborator_company_list;
+	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_back")
+	public WebElement b_addCollaborator_back;
+	@SearchWith(pageName = "addCollaborator", elementName = "b_addCollaborator_confirm")
+	public WebElement b_addCollaborator_confirm;
+	@SearchWith(pageName = "addCollaborator", elementName = "list_addCollaborator_changeOwner_1")
+	public WebElement list_addCollaborator_changeOwner_1;
+	@SearchWith(pageName = "addCollaborator", elementName = "addCollaborator_1_list_userEmail")
+	public WebElement addCollaborator_1_list_userEmail;
+
 }
