26c5b018357874ee0acb0d34c1d244066d6923b6
W55555 <1350334784@qq.com>
2018-03-13 17:01:32 +0800

    add

---
+++ src/cases/testCollection.java
@@ -0,0 +1,36 @@
+package cases;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import base.TestInit;
+
+public class testCollection extends TestInit {
+	/**
+	 * 文件夹添加协作
+	 * 
+	 * @author 王继程
+	 * @Time 2018-03-12
+	 *
+	 */
+	@Test(enabled = true)
+	public void Add_to() throws InterruptedException {
+//		login("Collection@shimo.im","123123");
+//		favorites.click();
+//		//hover文件
+//		wait.until(ExpectedConditions.elementToBeClickable(desktop1_1_folder));
+//		desktop1_1_folder.
+//		
+//		//点击齿轮
+//		//wait.until(ExpectedConditions.elementToBeClickable(desktop_setting));
+////		desktop_setting.click();
+//		
+//		//新标签页打开
+//		wait.until(ExpectedConditions.elementToBeClickable(desktop_setting_doc_1));
+//		desktop_setting_doc_1.click();
+//		
+		
+		
+	}
+//Desktop
+}
