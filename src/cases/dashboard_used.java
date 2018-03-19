f563f5ad1976cb65de50d28af787810e36585f5c
chenqingjie <1013328260@qq.com>
2018-03-19 14:17:13 +0800

    add

--- src/cases/dashboard_used.java
+++ src/cases/dashboard_used.java
@@ -12,6 +12,7 @@ import org.testng.annotations.Test;
 import com.gargoylesoftware.htmlunit.javascript.host.media.webkitMediaStream;
 
 import base.TestInit;
+import cases.testPermission;
 
 public class dashboard_used  extends TestInit {
 
@@ -50,7 +51,7 @@ public class dashboard_used  extends TestInit {
 	    public void used2() throws InterruptedException{
 		driver.navigate().to("https://release.shimodev.com");
 		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
-		login("zuijin1@shimo.im","123123" );
+		login("zuijin7@shimo.im","123123" );
 		wait.until(ExpectedConditions.elementToBeClickable(dashboard_1));
 		dashboard_2.click();
 		wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
@@ -88,7 +89,7 @@ public class dashboard_used  extends TestInit {
         	    
         	driver.navigate().to("https://release.shimodev.com");
     		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
-    		login("zuijin1@shimo.im","123123" );
+    		login("zuijin7@shimo.im","123123" );
     		wait.until(ExpectedConditions.elementToBeClickable(dashboard_1));
     		dashboard_2.click();
     		
@@ -97,31 +98,254 @@ public class dashboard_used  extends TestInit {
         WebElement icon = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
     		icon.click();
     		Thread.sleep(500);
-    		String text1 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li")).getText();
+    		String text1 = menu_newPage.getText();
     		assertEquals(text1, "在新标签页中打开");
-    	    String text2 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[2]")).getText();
+    	    String text2 = menu_mute.getText();
     	    assertEquals(text2, "消息免打扰");
-    	    String text3 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[3]")).getText();
+    	    String text3 = menu_shortcut.getText();
     	    assertEquals(text3, "添加到快捷方式");
-    	    String text4 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[4]")).getText();
+    	    String text4 = menu_collection.getText();
     	    assertEquals(text4, "收藏");
-    	    String text5 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[5]")).getText();
-    	    assertEquals(text5, "1 人协作");
-    	    String text6 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[6]")).getText();
-    	    assertEquals(text6, "移动");
-    	    String text7 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[7]")).getText();
-    	    assertEquals(text7, "定位到所在文件夹");
-    	    String text8 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[8]")).getText();
-    	    assertEquals(text8, "重命名");
+    	    String text6 = menu_cooperation.getText();
+    	    assertEquals(text6, "1 人协作");
+    	    String text7 = menu_move.getText();
+    	    assertEquals(text7, "移动");
+    	    String text9 = menu_moveToFolder.getText();
+    	    assertEquals(text9, "定位到所在文件夹");
+    	    String text10 = menu_rename.getText();
+    	    assertEquals(text10, "重命名");
+    	    String text11 = menu_creatCopy.getText();
+    	    assertEquals(text11, "创建副本");
+    	    String  text13 = menu_delete.getText();
+    	    assertEquals(text13, "删除");
     	    
-    	    
-    		
+    	   }
+        /**
+		 *最近使用
+		 * 卡片点点点，链接打开
+		 * @author 陈清杰
+		 * @Time2018-03-14
+		 *  **/
+        
+        @Test
+        public void used4() throws InterruptedException{
+        	driver.navigate().to("https://release.shimodev.com");
+    		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
+    		login("zuijin3@shimo.im","123123" );
+    		wait.until(ExpectedConditions.elementToBeClickable(dashboard_1));
+    	    driver.navigate().to("https://release.shimodev.com/docs/M5pgZFusSg8T1krl/");
+    	    wait.until(ExpectedConditions.elementToBeClickable(b_back));
+    	    b_back.click();
+    	    wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+    	    dashboard_2.click();
+    	    wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+    	    WebElement icon = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+    		icon.click();
+    		Thread.sleep(500);
+    		String text = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li")).getText();
+    		assertEquals(text, "在新标签页中打开");
+    		String text2 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[2]")).getText();
+    		assertEquals( text2, "收藏");
+    		String text4 = driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[4]")).getText();
+    		assertEquals(text4, "创建副本");
+    				
+          }
+        /**
+		 *最近使用
+		 * 卡片，最近打开的排在第一位
+		 * @author 陈清杰
+		 * @Time2018-03-14
+		 *  **/
+        @Test
+        public   void used5() throws InterruptedException{
+        	driver.navigate().to("http://release.shimodev.com");
+        	driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
+        	login("zuijin5@shimo.im", "123123");
+        	wait.until(ExpectedConditions.elementToBeClickable(dashboard_1));
+        	dashboard_2.click();
+        	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='content']//div//div//div//div//div//div[2]//div[3]//div//a")));
+       	WebElement icon2 = driver.findElement(By.xpath("//div[@id='content']//div//div//div//div//div//div[2]//div[3]//div//a"));
+       	icon2.click();
+       	wait.until(ExpectedConditions.elementToBeClickable(b_back));
+	    b_back.click();
+	    wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+	    dashboard_2.click();
+	    wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+	    String text = driver.findElement(By.xpath("//div[@id='content']//div//div//div//div//div//div[2]//div[2]//div//a//div//div//div[2]//div")).getText();
+	    assertEquals(text, "22222");
+	    
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='content']//div//div//div//div//div//div[2]//div[3]//div//a")));
+       	WebElement icon3 = driver.findElement(By.xpath("//div[@id='content']//div//div//div//div//div//div[2]//div[3]//div//a"));
+       	icon3.click();
+       	wait.until(ExpectedConditions.elementToBeClickable(b_back));
+	    b_back.click();
+	    wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+	    dashboard_2.click();
+	    wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+	    String text1 = driver.findElement(By.xpath("//div[@id='content']//div//div//div//div//div//div[2]//div[2]//div//a//div//div//div[2]//div")).getText();
+	    assertEquals(text1, "11111");
+		
+	    
+        	
+        }
+        /**
+		 *最近使用
+		 * 卡片，自己删除文件，最近使用对应卡片消失
+		 * @author 陈清杰
+		 * @Time2018-03-16
+		 *  **/
+        @Test
+        public void used6() throws InterruptedException{
+        	      login("zuijin8@shimo.im", "123123");
+        	      action.moveToElement(desktop_new).perform();
+        		  wait.until(ExpectedConditions.elementToBeClickable(desktop_newDoc));
+        		  desktop_newDoc.click();
+        		  wait.until(ExpectedConditions.elementToBeClickable(b_back));
+        		  b_back.click();
+        		  wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+        	      dashboard_2.click();
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+        	      WebElement icon = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+        	      icon.click();
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[13]")));
+        	      driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[13]")).click();
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='sm-modal-content']//div[3]//button[1]")));
+        	      driver.findElement(By.xpath("//div[@class='sm-modal-content']//div[3]//button[1]")).click();
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='none-file']//span")));
+        	      String text2 = driver.findElement(By.xpath("//div[@class='none-file']//span")).getText();
+        		  assertEquals(text2, "没有文件");
         	
         	
-	    	
-	    	
-	    	
-	    }
+        }
+        /**
+		 *最近使用
+		 * 卡片，自己退出共享，最近使用对应卡片消失
+		 * @author 陈清杰
+		 * @Time2018-03-16
+        *  **/
+        @Test
+        public void used7() throws InterruptedException{
+        	      login("zuijin9@shimo.im", "123123");
+        	      action.moveToElement(desktop_new).perform();
+        		  wait.until(ExpectedConditions.elementToBeClickable(desktop_newDoc));
+        		  desktop_newDoc.click();
+        		  wait.until(ExpectedConditions.elementToBeClickable(b_back));
+        		  b_back.click();
+        		  
+        		  wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+        	      WebElement icon = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+        	      icon.click();
+        	      Thread.sleep(500);
+        	      driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[6]")).click();
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(input_addCollaborator));
+        	      input_addCollaborator.sendKeys("zuijin8@shimo.im");
+        	      //验证添加按钮是否加载出来
+        	      wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_add));
+        	      b_addCollaborator_1_add.click();
+        	      logout();
+        	      login("zuijin8@shimo.im", "123123");
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='file-content file-text']")));
+        	      driver.findElement(By.xpath("//div[@class='file-content file-text']")).click();
+        	      wait.until(ExpectedConditions.elementToBeClickable(b_back));
+        		  b_back.click();
+        		  wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+        		  dashboard_2.click();
+        		  
+        		  wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+        	      WebElement icon1 = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+        	      icon1.click();
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[13]")));
+        	      driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[13]")).click();
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='sm-modal-footer']//button")));
+        	      driver.findElement(By.xpath("//div[@class='sm-modal-footer']//button")).click();
+        	      
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='none-file']//span")));
+        	      String text2 = driver.findElement(By.xpath("//div[@class='none-file']//span")).getText();
+        		  assertEquals(text2, "没有文件");
+        		  logout();
+        		  login("zuijin9@shimo.im", "123123");
+        		  
+        		  wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+        		  dashboard_2.click();
+        		  wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+        	      WebElement icon3 = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+        	      icon3.click();
+        	      wait.until(ExpectedConditions.elementToBeClickable(menu_delete));
+        	      menu_delete.click();
+        	      
+        	      WebElement icon4= driver.findElement(By.xpath("//div[@class='sm-modal-footer']//button[1]"));
+        	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='sm-modal-footer']//button[1]")));
+        	      icon4.click();
+        	     
+        	      
+        	      
+        	     }
+        /**
+		 *最近使用
+		 * 卡片，自己被移除协作
+		 * @author 陈清杰
+		 * @Time2018-03-19
+        *  **/
+        @Test
+        public void used8() throws InterruptedException{
+        	       login("zuijin9@shimo.im","123123");
+        	       action.moveToElement(desktop_new).perform();
+      		   wait.until(ExpectedConditions.elementToBeClickable(desktop_newDoc));
+      		   desktop_newDoc.click();
+               wait.until(ExpectedConditions.elementToBeClickable(b_back));
+      	       b_back.click();
+      	       wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+   	           dashboard_2.click();
+   	           wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+  	           WebElement icon = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+  	           icon.click();
+  	           Thread.sleep(500);
+  	           driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[6]")).click();
+   	           
+  	           wait.until(ExpectedConditions.elementToBeClickable(input_addCollaborator));
+   	           input_addCollaborator.sendKeys("zuijin8@shimo.im");
+   	           //验证添加按钮是否加载出来
+   	           wait.until(ExpectedConditions.elementToBeClickable(b_addCollaborator_1_add));
+   	           b_addCollaborator_1_add.click();
+   	           logout();  
+   	           login("zuijin8@shimo.im", "123123");
+   	           wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='file-content file-text']")));
+  	           driver.findElement(By.xpath("//div[@class='file-content file-text']")).click();
+  	           wait.until(ExpectedConditions.elementToBeClickable(b_back));
+  		       logout();
+  		       
+  		       login("zuijin9@shimo.im", "123123");
+  		       wait.until(ExpectedConditions.elementToBeClickable(dashboard_2));
+ 	           dashboard_2.click();
+ 	           wait.until(ExpectedConditions.elementToBeClickable(dashboard_update_file));
+    	           WebElement icon1 = driver.findElement(By.xpath("//div[@class='file-options-icon']"));
+    	           icon1.click();  
+    	           Thread.sleep(500);
+       	       driver.findElement(By.xpath("//div[@class='category-card-container']//div//div[2]//div//div//ul//li[6]")).click();
+    	      
+       	       wait.until(ExpectedConditions.elementToBeClickable(input_addCollaborator));
+       	       
+ 	           
+   	             
+  		 
+   	           
+   	           
+   	           
+   	            }
+        
+		
+
+        	
+        
+        
+        
+        
 	
 	
 	
--- src/cases/login_error.java
+++ src/cases/login_error.java
@@ -4,43 +4,40 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.server.handler.FindElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 import base.TestInit;
 
-public class login_error  extends TestInit{
-	
+public class login_error extends TestInit {
+
 	/**
-	 *登录
-	 * 输入不符合格式的邮箱登录
+	 * 登录 输入不符合格式的邮箱登录
+	 * 
 	 * @author 陈清杰
 	 * @Time 2018-01-08
 	 */
-	
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void   login_error() throws InterruptedException {
-		
+	public login_error() throws InterruptedException {
+
 		driver.navigate().to("https://release.shimodev.com/");
-		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
-		
+		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"))
+				.click();
+
 		login_error("chenqingjie", "123123");
-		wait.until(ExpectedConditions.textToBePresentInElement(By.className("error-block"), "请输入一个有效的邮箱地址"));
-	    String toast = "";
-	    toast = driver.findElement(By.className("error-block")).getText();
+		wait.until(ExpectedConditions.textToBe(By.className("error-block"), "请输入一个有效的邮箱地址"));
+		String toast = "";
+		toast = driver.findElement(By.className("error-block")).getText();
 
 		assertEquals(toast, "请输入一个有效的邮箱地址");
-		
-		
-				
+
 	}
-	
+
 	/**
-	 * 登录
-	 * 邮箱输入为空注册
+	 * 登录 邮箱输入为空注册
+	 * 
 	 * @author 陈清杰
 	 * @Time 2018-01-08
 	 *
@@ -48,142 +45,138 @@ public class login_error  extends TestInit{
 	@SuppressWarnings("deprecation")
 	@Test
 	public void login_error0() throws InterruptedException {
-		
+
 		driver.navigate().to("https://release.shimodev.com/");
-		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
-		
-		login_error("","123123");
-		wait.until(ExpectedConditions.textToBePresentInElement(By.className("error-block"), "请输入邮箱"));
-		
+		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"))
+				.click();
+
+		login_error("", "123123");
+		wait.until(ExpectedConditions.textToBe(By.className("error-block"), "请输入邮箱"));
+
 		String toast = "";
 		toast = driver.findElement(By.className("error-block")).getText();
 		assertEquals(toast, "请输入邮箱");
-		
+
 	}
-	
+
 	/**
-	 * 登录
-	 *输入未注册的邮箱登录
+	 * 登录 输入未注册的邮箱登录
+	 * 
 	 * @author 陈清杰
 	 * @Time 2018-01-08
 	 *
 	 */
 	@Test
-	public void login_error1() throws InterruptedException{
+	public void login_error1() throws InterruptedException {
 		driver.navigate().to("https://release.shimodev.com/");
-		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']")).click();
-		
-		login_error("nizhidaomilandexiaotiejiangma@qq.com","123123");
-		wait.until(ExpectedConditions.textToBePresentInElement(By.className("error-block"),"用户不存在"));
-		
+		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"))
+				.click();
+
+		login_error("nizhidaomilandexiaotiejiangma@qq.com", "123123");
+		wait.until(ExpectedConditions.textToBe(By.className("error-block"), "用户不存在"));
+
 		String toast = "";
 		toast = driver.findElement(By.className("error-block")).getText();
 		assertEquals(toast, "用户不存在");
 	}
-	
+
 	/**
-	 * 登录
-	 *密码输入为空
+	 * 登录 密码输入为空
+	 * ss
 	 * @author 陈清杰
 	 * @Time 2018-01-08
 	 *
 	 */
 	@SuppressWarnings("deprecation")
 	@Test
-	public void login_error2 ()throws InterruptedException{
+	public void login_error2() throws InterruptedException {
 		driver.navigate().to("https://release.shimodev.com/");
 		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"));
-		
-		login_error("chenqingjie@shimo.im","");
-		wait.until(ExpectedConditions.textToBePresentInElement(By.className("error-block"), "请输入密码"));
-		
-		
+
+		login_error("chenqingjie@shimo.im", "");
+		wait.until(ExpectedConditions.textToBe(By.className("error-block"), "请输入密码"));
+
 		String toast = "";
 		toast = driver.findElement(By.className("error-block")).getText();
 		assertEquals(toast, "请输入密码");
-		
-		}
-	
+
+	}
+
 	/**
-	 * 登录
-	 *密码输入为空格
+	 * 登录 密码输入为空格
+	 * 
 	 * @author 陈清杰
 	 * @Time 2018-01-08
 	 *
 	 */
 	@SuppressWarnings("deprecation")
 	@Test
-	public void login_error3()throws Exception{
-		
+	public void login_error3() throws Exception {
+
 		driver.navigate().to("https://release.shimodev.com/");
 		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"));
-		
-		login_error("chenqingjie@shimo.im"," ");
-		wait.until(ExpectedConditions.textToBePresentInElement(By.className("error-block"), "密码有误"));
-		
+
+		login_error("chenqingjie@shimo.im", " ");
+		wait.until(ExpectedConditions.textToBe(By.className("error-block"), "密码有误"));
+
 		String toast = "";
 		toast = driver.findElement(By.className("error-block")).getText();
 		assertEquals(toast, "密码有误");
 	}
-	
+
 	/**
-	 * 登录
-	 *密码输入错误
+	 * 登录 密码输入错误
+	 * 
 	 * @author 陈清杰
 	 * @Time 2018-01-08
 	 *
 	 */
 	@SuppressWarnings("deprecation")
 	@Test
-	public void login_error4() throws Exception{
-		
+	public void login_error4() throws Exception {
+
 		driver.navigate().to("https://release.shimodev.com");
 		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"));
-		
-		login_error("chenqingjie@shimo.im","8294854");
-		wait.until(ExpectedConditions.textToBePresentInElement(By.className("error-block"), "密码有误"));
-		
+
+		login_error("chenqingjie@shimo.im", "8294854");
+		wait.until(ExpectedConditions.textToBe(By.className("error-block"), "密码有误"));
+
 		String toast = "";
-	    toast = driver.findElement(By.className("error-block")).getText();
-	    assertEquals(toast, "密码有误");
+		toast = driver.findElement(By.className("error-block")).getText();
+		assertEquals(toast, "密码有误");
 	}
+
 	/**
-	 * 登录
-	 *密码连续输错
+	 * 登录 密码连续输错
+	 * 
 	 * @author 陈清杰
 	 * @Time 2018-01-09
 	 *
 	 */
 	@Test
-	public void login_error5() throws Exception{
-		
+	public void login_error5() throws Exception {
+
 		driver.navigate().to("https://release.shimodev.com/");
 		driver.findElement(By.xpath("//li[@class='home-nav-item']//a[@class='home-button btn-middle login-shimo']"));
-		
+
 		login_error("chenqingjie@shimo.im", "777777");
-//		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.className("error-block"), "密码有误"));
-		
+		// wait.until(ExpectedConditions.textToBePresentInElementLocated(By.className("error-block"),
+		// "密码有误"));
+
 		String msg = driver.findElement(By.className("error-block")).getText();
-		
-		for(int n=0;n<20;n++) {
-			if(msg != "验证码错误") {
+
+		for (int n = 0; n < 20; n++) {
+			if (msg != "验证码错误") {
 				userPwd.sendKeys("03940984");
 				login_submit.click();
 				msg = driver.findElement(By.className("error-block")).getText();
-			}else {
+			} else {
 				break;
 			}
 		}
 		Boolean ii = driver.findElement(By.xpath("//div[@class='inline']//div[2]")).isDisplayed();
-        assertTrue(ii);
-		
+		assertTrue(ii);
+
 	}
-	
-	
-	
-	
-	
-	
 
 }
-
