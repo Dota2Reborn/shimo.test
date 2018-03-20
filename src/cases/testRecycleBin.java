50f1ce50895f262925d6542d080114ea1ea1eb01
W55555 <1350334784@qq.com>
2018-03-20 18:14:08 +0800

    update

--- src/cases/testRecycleBin.java
+++ src/cases/testRecycleBin.java
@@ -1,8 +1,25 @@
 package cases;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
 import base.TestInit;
 
 public class testRecycleBin extends TestInit{
-	
+	/**
+	 * 新建文档收藏
+	 * 
+	 * @author 王继程
+	 * @Time 2018-03-12
+	 *
+	 */
+	@Test(enabled = true)
+	public void New_Document() throws InterruptedException {
+		login("RecycleBin@shimo.im", "123123");
+		
+		
+	}
 
 }
