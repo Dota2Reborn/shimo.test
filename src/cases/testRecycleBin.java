00188e20d4d76dff0833bf7a752a91e0c964110c
W55555 <1350334784@qq.com>
2018-03-21 13:13:00 +0800

    update

--- src/cases/testRecycleBin.java
+++ src/cases/testRecycleBin.java
@@ -2,6 +2,7 @@ package cases;
 
 import static org.testng.Assert.assertEquals;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
@@ -57,12 +58,13 @@ public class testRecycleBin extends TestInit{
 		desktop_newDoc.click();
 		wait.until(ExpectedConditions.elementToBeClickable(doc_edit));
 		doc_edit.sendKeys("文档内输入内容，可进入回收站");
+		Thread.sleep(1000);
 		//点点点
 		doc_menu.click();
 		//删除按钮
 		wait.until(ExpectedConditions.elementToBeClickable(file_menu_delete));
 		file_menu_delete.click();
-		//删除文档，确认删除（存在问题）
+		//删除文档，确认删除
 		wait.until(ExpectedConditions.elementToBeClickable(doc_menu_delete_OK));
 		doc_menu_delete_OK.click();
 		wait.until(ExpectedConditions.elementToBeClickable(trash));
@@ -73,6 +75,11 @@ public class testRecycleBin extends TestInit{
 		action.contextClick(desktop1_1).perform();
 		wait.until(ExpectedConditions.elementToBeClickable(menu_Completely_removed));
 		menu_Completely_removed.click();
+		wait.until(ExpectedConditions.elementToBeClickable(desktop_newFolder_name_ok));
+		desktop_newFolder_name_ok.click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.className("none-file")));
+		boolean bl = driver.findElement(By.className("none-file")).isDisplayed();
+		assertEquals(bl,true);
 		
 	}
 
