27895479cac07aea9dd42d310503d69ec5c68d1f
W55555 <1350334784@qq.com>
2018-03-27 16:04:44 +0800

    update

--- src/cases/testAlertMute.java
+++ src/cases/testAlertMute.java
@@ -19,6 +19,8 @@ public class testAlertMute extends TestInit {
 	/**
 	 * 关闭消息免打扰，协作这评论，收到消息通知
 	 * 
+	 * 已开启免打扰AlertMute@shimo.im
+	 * 
 	 * @author 王继程
 	 * @Time 2018-03-23
 	 *
@@ -27,9 +29,8 @@ public class testAlertMute extends TestInit {
 	public void Notify_Open() throws InterruptedException {
 		login("AlertMute@shimo.im", "123123");
 		click(dashboard_2);
-		
 		click(menu_Point_Menu);
-		click(menu_mute);
+		click(menu_mute);//接收消息通知
 		logout();
 		login("AlertMute1@shimo.im", "123123");
 		click(desktop);
@@ -69,11 +70,13 @@ public class testAlertMute extends TestInit {
 		click(End_Discussion);
 		click(End_Discussion_OK);
 		click(doc_menu);
-		click(file_menu_mute);
+		click(file_menu_mute);//关闭消息通知
 		
 	}
 	/**
 	 * 开启消息免打扰，在关闭消息免打扰，验证最近更新
+	 * 
+	 * 未开启免打扰AlertMute2@shimo.im
 	 * 
 	 * @author 王继程
 	 * @Time 2018-03-23
@@ -84,25 +87,26 @@ public class testAlertMute extends TestInit {
 		login("AlertMute2@shimo.im", "123123");
 		click(dashboard_1);
 		click(menu_Point_Menu);
-		click(menu_mute);
+		click(menu_mute);//关闭消息通知
 		driver.navigate().refresh();
-		Boolean exist = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[2]"));
+		Boolean exist = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[@class='category-card-container']"));
 		assertFalse(exist);
 		click(dashboard_2);
 		click(menu_Point_Menu);
-		click(menu_mute);
+		click(menu_mute);//开启消息通知
 		dashboard_share_file.click();
 		wait.until(ExpectedConditions.elementToBeClickable(doc_edit));
 		doc_edit.sendKeys(Keys.CONTROL,'a'+"编辑文档并保存"+Keys.ENTER);
+		Thread.sleep(500);
 		click(quick_access_point);
 		click(Back_to_Table);
 		wait.until(ExpectedConditions.elementToBeClickable(dashboard));
-		Boolean exist1 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[2]"));
+		Boolean exist1 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[@class='category-card-container']"));
 		int i=1;
         while(exist1==false&&i<6){
         	driver.navigate().refresh();
         	wait.until(ExpectedConditions.elementToBeClickable(dashboard));
-            exist1 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[2]"));
+            exist1 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[@class='category-card-container']"));
             i++;
         }
         if(exist1=true) {
@@ -114,6 +118,8 @@ public class testAlertMute extends TestInit {
 	/**
 	 * 关闭消息免打扰，协作者编辑文档，出现在最近更新中
 	 * 
+	 * AlertMute3@shimo.im已开启免打扰
+	 * 
 	 * @author 王继程
 	 * @Time 2018-03-23
 	 *
@@ -123,35 +129,35 @@ public class testAlertMute extends TestInit {
 		login("AlertMute3@shimo.im", "123123");
 		click(dashboard_2);
 		click(menu_Point_Menu);
-		click(menu_mute);
+		click(menu_mute);//开启消息通知
 		logout();
 		login("AlertMute4@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
 		//已经打开文档
-		click(doc_edit);
+		wait.until(ExpectedConditions.elementToBeClickable(doc_edit));
 		doc_edit.sendKeys(Keys.CONTROL,'a'+"编辑文档并保存"+Keys.ENTER);
 		Thread.sleep(500);
 		logout();
 		login("AlertMute3@shimo.im", "123123");
-		click(dashboard);
-		Boolean exist1 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[2]"));
+		wait.until(ExpectedConditions.elementToBeClickable(dashboard));
+		Boolean exist2 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[@class='category-card-container']"));
 		int i=1;
-        while(exist1==false&&i<6){
+        while(exist2==false&&i<6){
         	driver.navigate().refresh();
-        	wait.until(ExpectedConditions.elementToBeClickable(menu_Point_Menu));
-            exist1 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[2]"));
+        	wait.until(ExpectedConditions.elementToBeClickable(dashboard));
+            exist2 = doesWebElementExist(By.xpath("//div[@data-test='category-list-wrapper']//div[2]//div[@class='category-card-container']"));
             i++;
         }
-        if(exist1=true) {
+        if(exist2=true) {
         	click(menu_Point_Menu);
-        	click(menu_mute);
-        	assertTrue(exist1);
+        	click(menu_mute);//关闭消息通知
+        	assertTrue(exist2);
         }else {
         	System.out.println("接收消息提醒验证未通过，请登录AlertMute3@shimo.im自行验证");
         	click(dashboard_2);
         	click(menu_Point_Menu);
-        	click(menu_mute);
+        	click(menu_mute);//关闭消息通知
         }
 	}
 	
