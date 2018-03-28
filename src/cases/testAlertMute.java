815b8693d3a91e545665b1402e0a151136fedb2b
W55555 <1350334784@qq.com>
2018-03-28 11:03:07 +0800

    update

--- src/cases/testAlertMute.java
+++ src/cases/testAlertMute.java
@@ -169,8 +169,12 @@ public class testAlertMute extends TestInit {
 	@Test(enabled = true)
 	public void Folder_notifications() throws InterruptedException {
 		login("AlertMute5@shimo.im", "123123");
-		click(desktop);
+		clickDesktop();
 		click(desktop1_1);
+		contextClick(desktop1_1);
+		click(menu_mute);
+		
+		
 		
 	}
 	
