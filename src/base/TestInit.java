b2f17d122a0e041ad829cbfd0b6cc7e318e2b0fa
Dota2Reborn <zhuiyaoyan@126.com>
2018-05-07 16:07:30 +0800

    update

--- src/base/TestInit.java
+++ src/base/TestInit.java
@@ -21,7 +21,6 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
