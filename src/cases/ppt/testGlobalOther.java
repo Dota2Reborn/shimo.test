e6d0401bbc2c91bc631325bfc9150cd71c0e1553
W55555 <1350334784@qq.com>
2018-07-30 09:10:54 +0800

    update

--- src/cases/ppt/testGlobalOther.java
+++ src/cases/ppt/testGlobalOther.java
@@ -1,5 +1,37 @@
 package cases.ppt;
 
-public class testGlobalOther {
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import base.TestInit;
+
+public class testGlobalOther extends TestInit {
+	/**
+	 * 页面缩放调整
+	 * 
+	 * @author 王继程
+	 * @Time 2018-07-23
+	 *
+	 */
+	@Test(enabled = true)
+	public void Page_Style() throws InterruptedException {
+		login("pagestyle@shimo.im", "123123");
+		click(desktop);
+		click(desktop1_1);
+		for(int i=1;i<5;i++) {
+			click(narrow_trying);
+		}
+		
+		String zomm1 = getText(ppt_zoom);
+		driver.navigate().refresh();
+		for(int i=1;i<5;i++) {
+			click(enlarge_trying);
+		}
+		String zomm2 = getText(ppt_zoom);
+		assertEquals(zomm1,"25%");
+		assertEquals(zomm2,"400%");
+		
+	}
 
 }
