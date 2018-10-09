1e05335e2aefeac36d9004575ba7893e82e473a2
Dota2Reborn <zhuiyaoyan@126.com>
2018-10-09 18:24:25 +0800

    update

--- src/base/baseFunc.java
+++ src/base/baseFunc.java
@@ -26,118 +26,116 @@ import elementFile.CustomFieldDecorator;
 import elementFile.MyElementLocatorFactory;
 
 public class baseFunc {
-	private int environment;// 切换测试环境
-	private String test_url;// 测试地址
-	public WebDriver driver;
-	String os;// 系统信息
+    private int environment;// 切换测试环境
+    private String test_url;// 测试地址
+    public WebDriver driver;
+    String os;// 系统信息
 
-	public baseFunc() {
-		environment = 2; // 1为release 2为dev
-	}
+    public baseFunc() {
+        environment = 2; // 1为release 2为dev
+    }
 
-	/**
-	 * 加载json
-	 * 
-	 * @author 刘晨
-	 * @throws MalformedURLException
-	 * @Time 2017-11-20
-	 *
-	 */
-	public WebDriver initData(Object xx, String browser, String nodeIp, String local) throws MalformedURLException {
-		if (local.equals("true")) {
-			os = System.getProperties().getProperty("os.name");
-			Preconditions.checkArgument(StringUtils.isNotEmpty(os), "OS info is missing.");
-			if (os.startsWith("Windows")&&browser.equals("chrome")) {
-				System.setProperty("webdriver.chrome.driver",
-						"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+    /**
+     * 加载json
+     *
+     * @throws MalformedURLException
+     * @author 刘晨
+     * @Time 2017-11-20
+     */
+    public WebDriver initData(Object xx, String browser, String nodeIp, String local) throws MalformedURLException {
+        if (local.equals("true")) {
+            os = System.getProperties().getProperty("os.name");
+            Preconditions.checkArgument(StringUtils.isNotEmpty(os), "OS info is missing.");
+            if (os.startsWith("Windows") && browser.equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver",
+                        "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
 
-				ChromeOptions option = new ChromeOptions();
-				option.addArguments("disable-infobars");
+                ChromeOptions option = new ChromeOptions();
+                option.addArguments("disable-infobars");
 //				option.addArguments("--headless");
-				driver = new ChromeDriver(option);
-			} else if(os.startsWith("Windows")&&browser.equals("firefox")){
-				System.setProperty("webdriver.gecko.driver",
-						"D:\\软件\\Mozilla Firefox\\geckodriver.exe");
-				FirefoxOptions option = new FirefoxOptions();
-				option.addArguments("disable-infobars");
-				option.addPreference("browser.startup.page", 1);
-				option.addPreference("browser.startup.homepage", "about:blank");
+                driver = new ChromeDriver(option);
+            } else if (os.startsWith("Windows") && browser.equals("firefox")) {
+                System.setProperty("webdriver.gecko.driver",
+                        "D:\\软件\\Mozilla Firefox\\geckodriver.exe");
+                FirefoxOptions option = new FirefoxOptions();
+                option.addArguments("disable-infobars");
+                option.addPreference("browser.startup.page", 1);
+                option.addPreference("browser.startup.homepage", "about:blank");
 //				option.setHeadless(true);
-				ProfilesIni pi = new ProfilesIni();
+                ProfilesIni pi = new ProfilesIni();
 //				File profile_directory = new File("C:\\Users\\LiuC\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\95djexwa.default");
 //				FirefoxProfile profile = new FirefoxProfile(profile_directory);
-				FirefoxProfile profile = pi.getProfile("default");
-				option.setProfile(profile);
-				option.setLogLevel(FirefoxDriverLogLevel.ERROR);
-				option.setBinary("D:\\软件\\Mozilla Firefox\\firefox.exe");
-				driver = new FirefoxDriver(option);
-			}else {
-				ChromeOptions option = new ChromeOptions();
-				option.addArguments("disable-infobars");
-				driver = new ChromeDriver(option);
-			}
-		} else {
-			if (browser.equals("ie")) {
-				InternetExplorerOptions option = new InternetExplorerOptions();
-				option.setCapability("browserName", "IE");
-				option.setCapability("version", "11");
-				option.setCapability("plaform", "WINDOWS");
-				URL remoteAddress = new URL(nodeIp + "/wd/hub");
-				driver = new RemoteWebDriver(remoteAddress, option);
-			} else if (browser.equals("safari")) {
-				SafariOptions option = new SafariOptions();
-				option.setCapability("browserName", "safari");
-				option.setCapability("version", "11");
-				option.setCapability("plaform", "MAC");
-				option.setCapability("javascriptEnabled", true);
-				option.useCleanSession(true);
-				URL remoteAddress = new URL(nodeIp + "/wd/hub");
-				driver = new RemoteWebDriver(remoteAddress, option);
-			} else if (browser.equals("firefox")) {
-				FirefoxOptions option = new FirefoxOptions();
-				option.addArguments("disable-infobars");
-				option.setCapability("browserName", "firefox");
-				option.setCapability("version", "60");
-				option.setCapability("plaform", "ANY");
-				URL remoteAddress = new URL(nodeIp + "/wd/hub");
-				driver = new RemoteWebDriver(remoteAddress, option);
-			} else {
-				ChromeOptions option = new ChromeOptions();
-				option.addArguments("disable-infobars");
+                FirefoxProfile profile = pi.getProfile("default");
+                option.setProfile(profile);
+                option.setLogLevel(FirefoxDriverLogLevel.ERROR);
+                option.setBinary("D:\\软件\\Mozilla Firefox\\firefox.exe");
+                driver = new FirefoxDriver(option);
+            } else {
+                ChromeOptions option = new ChromeOptions();
+                option.addArguments("disable-infobars");
+                driver = new ChromeDriver(option);
+            }
+        } else {
+            if (browser.equals("ie")) {
+                InternetExplorerOptions option = new InternetExplorerOptions();
+                option.setCapability("browserName", "IE");
+                option.setCapability("version", "11");
+                option.setCapability("plaform", "WINDOWS");
+                URL remoteAddress = new URL(nodeIp + "/wd/hub");
+                driver = new RemoteWebDriver(remoteAddress, option);
+            } else if (browser.equals("safari")) {
+                SafariOptions option = new SafariOptions();
+                option.setCapability("browserName", "safari");
+                option.setCapability("version", "11");
+                option.setCapability("plaform", "MAC");
+                option.setCapability("javascriptEnabled", true);
+                option.useCleanSession(true);
+                URL remoteAddress = new URL(nodeIp + "/wd/hub");
+                driver = new RemoteWebDriver(remoteAddress, option);
+            } else if (browser.equals("firefox")) {
+                FirefoxOptions option = new FirefoxOptions();
+                option.addArguments("disable-infobars");
+                option.setCapability("browserName", "firefox");
+                option.setCapability("version", "60");
+                option.setCapability("plaform", "ANY");
+                URL remoteAddress = new URL(nodeIp + "/wd/hub");
+                driver = new RemoteWebDriver(remoteAddress, option);
+            } else {
+                ChromeOptions option = new ChromeOptions();
+                option.addArguments("disable-infobars");
 //				option.addArguments("--headless");
-				// option.addArguments("--kiosk");
-				option.setCapability("browserName", "chrome");
-				option.setCapability("version", "67");
-				option.setCapability("plaform", "ANY");
-				URL remoteAddress = new URL(nodeIp + "/wd/hub");
-				driver = new RemoteWebDriver(remoteAddress, option);
-			}
-			// option.setCapability("chrome_binary", "C:\\Program Files
-			// (x86)\\Google\\Chrome\\Application\\");
-		}
+                // option.addArguments("--kiosk");
+                option.setCapability("browserName", "chrome");
+                option.setCapability("version", "67");
+                option.setCapability("plaform", "ANY");
+                URL remoteAddress = new URL(nodeIp + "/wd/hub");
+                driver = new RemoteWebDriver(remoteAddress, option);
+            }
+            // option.setCapability("chrome_binary", "C:\\Program Files
+            // (x86)\\Google\\Chrome\\Application\\");
+        }
 
-		driver.manage().window().maximize();
-		ElementLocatorFactory locatorFactory = new MyElementLocatorFactory(driver);
-		FieldDecorator customFieldDecorator = new CustomFieldDecorator(locatorFactory);
-		PageFactory.initElements(customFieldDecorator, xx);
-		return driver;
+        driver.manage().window().maximize();
+        ElementLocatorFactory locatorFactory = new MyElementLocatorFactory(driver);
+        FieldDecorator customFieldDecorator = new CustomFieldDecorator(locatorFactory);
+        PageFactory.initElements(customFieldDecorator, xx);
+        return driver;
 
-	}
+    }
 
-	/**
-	 * 获取测试地址
-	 * 
-	 * @author 刘晨
-	 * @Time 2017-11-20
-	 *
-	 */
-	public String getUrl() {
-		if (environment == 1) {
-			test_url = "https://release.shimodev.com/";
-		} else if (environment == 2) {
-			test_url = "https://shimodev.com/";
-		}
-		return test_url;
-	}
+    /**
+     * 获取测试地址
+     *
+     * @author 刘晨
+     * @Time 2017-11-20
+     */
+    public String getUrl() {
+        if (environment == 1) {
+            test_url = "https://release.shimodev.com/";
+        } else if (environment == 2) {
+            test_url = "https://shimodev.com/";
+        }
+        return test_url;
+    }
 
 }
