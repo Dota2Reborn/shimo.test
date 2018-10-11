efb1f1d4e74d2ced76043d00aeb28185375dfef3
W55555 <1350334784@qq.com>
2018-10-11 11:11:54 +0800

    update

--- src/cases/ppt/testGlobalPage.java
+++ src/cases/ppt/testGlobalPage.java
@@ -2,6 +2,8 @@ package cases.ppt;
 
 import static org.testng.Assert.assertTrue;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -22,13 +24,13 @@ public class testGlobalPage extends TestInit {//全局页面
 		login("NewTemplate@shimo.im", "123123");
 		click(desktop_new);
 		click(desktop_newslides);
-		for (int i = 1; i < 9; i++) {
+		for (int i = 1; i < 11; i++) {
 			click(down_arrow);
 			WebElement template = driver
 					.findElement(By.xpath("//ul[@id='sm-slide-layout']//li["+i+"]"));
 			click(template);
 		}
-		Boolean result = doesWebElementExist(ppt_page_9);
+		Boolean result = doesWebElementExist(ppt_page_11);
 		click(ppt_dotdotdot);
 		click(ppt_delete);
 		click(desktop_newFolder_name_ok);
@@ -82,22 +84,49 @@ public class testGlobalPage extends TestInit {//全局页面
 	 * 
 	 * @author 王继程
 	 * @Time 2018-07-23
-	 *
+	 *false
 	 */
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void Sorting_Order() throws InterruptedException {
 		login("SortingOrder@shimo.im", "123123");
 		click(desktop);
 		click(desktop1_1);
 		click(ppt_page_1);
-		moveToElement(ppt_page_1);
-//		action.clickAndHold(ppt_page_1).build().perform();
-//		moveToElement(ppt_page_5);
-//		action.release().perform();
-		int px=-100;
-		action.clickAndHold(ppt_page_1).build().perform();
-		action.dragAndDropBy(ppt_page_1, 0, px).build().perform();
-		//action.dragAndDropBy(ppt_page_1, 0, 100).perform();
+
+		click(page_elements_1);
+		click(page_elements_2);
+		click(page_elements_3);
+		click(page_elements_4);
+		click(page_elements_5);
+		click(page_elements_1);
+		click(page_elements_2);
+		click(page_elements_3);
+		click(page_elements_4);
+		click(page_elements_5);
+		click(page_elements_1);
+		click(page_elements_2);
+		click(page_elements_3);
+		click(page_elements_4);
+		click(page_elements_5);
+		click(ppt_page_1);
+		click(ppt_page_2);
+		click(ppt_page_3);
+		click(ppt_page_4);
+		click(ppt_page_5);
+		click(ppt_page_6);
+		click(ppt_page_7);
+		click(ppt_page_8);
+		click(ppt_page_1);
+		click(ppt_page_3);
+		click(ppt_page_4);
+		click(ppt_page_5);
+		click(ppt_page_6);
+		click(ppt_page_7);
+
+	}
+	public void Page_elements(int dian) {
+		int intArray[] = {0,1,2,3,4};
+		
 	}
 
 }
