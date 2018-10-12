a4dbdc094684bc02448423bae8fa4a76f033fb4f
W55555 <1350334784@qq.com>
2018-10-12 10:10:40 +0800

    update

--- src/elementFile/elementFile.java
+++ src/elementFile/elementFile.java
@@ -544,14 +544,6 @@ public class elementFile {
 	public WebElement page_elements_4;
 	@SearchWith(pageName = "ppt_page", elementName = "page_elements_5", noteName = "幻灯片页面第五个元素")
 	public WebElement page_elements_5;
-	@SearchWith(pageName = "ppt_page", elementName = "page_elements_select", noteName = "幻灯片页面选中的第一个元素")
-	public WebElement page_elements_select;
-	@SearchWith(pageName = "ppt_page", elementName = "page_elements_special1", noteName = "幻灯片特殊情况下第一个元素")
-	public WebElement page_elements_special1;
-	@SearchWith(pageName = "ppt_page", elementName = "page_elements_special2", noteName = "幻灯片特殊情况下第二个元素")
-	public WebElement page_elements_special2;
-	@SearchWith(pageName = "ppt_page", elementName = "page_elements_special3", noteName = "幻灯片特殊情况下第三个元素")
-	public WebElement page_elements_special3;
 	@SearchWith(pageName = "ppt_page", elementName = "page_elements_1_1", noteName = "图片占位符1上传图片按钮")
 	public WebElement page_elements_1_1;
 	@SearchWith(pageName = "ppt_page", elementName = "page_elements_2_1", noteName = "图片占位符2上传图片按钮")
